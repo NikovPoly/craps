7c84370668ddc286beb39b848e6ed6140590048a
Niko Vidalakis <nvidalakis23@students.polytechnic.org>
2022-01-18 13:17:05 -0800

    Finished die class

---
+++ Craps.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Craps {
+    
+
+    public static void main(String[] args) {
+        Die d1 = new Die();
+
+       
+        
+    }
+}
--- Die.java
+++ Die.java
@@ -1,4 +1,29 @@
 public class Die{
 
-    
+    //fields
+    private int num1;
+    private int num2;
+
+    //constructor
+    public Die(){
+
+        num1= (int)(Math.random()* 6) + 1;
+        num2 = (int)(Math.random()* 6) + 1;
+
+    }
+
+
+    //methods
+    public int roll(){
+
+        num1= (int)(Math.random()* 6) + 1;
+        num2= (int)(Math.random()* 6) + 1;
+
+        return num1+num2;
+
+        
+
+    }
+
+
 }
